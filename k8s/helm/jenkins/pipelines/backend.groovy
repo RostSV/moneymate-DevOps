7e6356a2948d055a9b2c1f15ba364d3fc86a3777
Rostyslav Svistula <rostyslav.svistula@student.tuke.sk>
2024-04-27 00:51:07 +0200

    implementing jenkins & k8s

---
+++ k8s/helm/jenkins/pipelines/backend.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+    environment {
+        CONTAINER_REGISTRY = 'https://fsa2024rsvistularegistry.azurecr.io'
+        DOCKER_REPOSITORY = 'fsa2024rsvistularegistry.azurecr.io'
+        IMAGE_NAME = 'fsa-backend'
+    }
+
+    stages {
+        stage('Clean workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Sleep') {
+            steps {
+                sh 'sleep 1s'
+            }
+        }
+        stage('Checkout SCM') {
+            steps {
+                git credentialsId: 'github-credentials', branch: 'main', url: 'https://github.com/RostSV/finance-tracker-app'
+                script {
+                    gitCommitId = sh(returnStdout: true, script: "git rev-parse --short HEAD").trim()
+                    IMAGE_VERSION = sh(returnStdout: true, script: "echo ${BUILD_ID}-${gitCommitId}").trim()         
+                }
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                container('dind') {
+                    withCredentials([usernamePassword(credentialsId: 'acr-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ${CONTAINER_REGISTRY}'
+                    }
+                    sh "docker build -t ${DOCKER_REPOSITORY}/${IMAGE_NAME}:${IMAGE_VERSION} ."
+                    sh "docker push ${DOCKER_REPOSITORY}/${IMAGE_NAME}:${IMAGE_VERSION}"
+                }
+            }
+        }
+    }
+}
